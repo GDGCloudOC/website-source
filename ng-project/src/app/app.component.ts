cce540370ba372b62eeb3c4ac2fa5a152cd6b44d
Charlie Collier <charlie.collier@neudesic.com>
2019-05-24 16:16:30 -0700

    merging changes

--- ng-project/src/app/app.component.ts
+++ ng-project/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'GDG Cloud OC';
+  title = "ng-project";
 }
--- ng-project/src/app/app.module.ts
+++ ng-project/src/app/app.module.ts
@@ -1,24 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { EventsComponent } from './events/events.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MaterialModule } from "./material/material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { LayoutComponent } from "./layout/layout.component";
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./navigation/header/header.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    LayoutComponent,
     HomeComponent,
-    EventsComponent,
-    NavigationComponent
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
