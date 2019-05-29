0807c63d5df1ee9dd134baaadbe34aa1c0069b3d
Mikkel Sandberg <mikkel.sandberg@neudesic.com>
2019-05-29 16:58:39 -0700

    adding contact form skeleton

--- ng-project/src/app/contact/contact.component.ts
+++ ng-project/src/app/contact/contact.component.ts
@@ -1,18 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "contact-view",
-  templateUrl: "contact.component.html",
-  styles: [
-    `
-      .contact-card {
-        margin-top: 20px;
-      }
-      .contact-card p {
-        padding-top: 10px;
-      }
-    `
-  ]
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
   constructor() {}
