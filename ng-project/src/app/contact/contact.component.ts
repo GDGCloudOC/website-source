67780149d31a67d90769a27f5b0b37dfc58b7023
Mikkel Sandberg <mikkel.sandberg@neudesic.com>
2019-05-30 10:56:59 -0700

    adding styles for contact form

--- ng-project/src/app/contact/contact.component.ts
+++ ng-project/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  env = environment;
+
   constructor() {}
 
   ngOnInit() {}
--- ng-project/src/app/photos/photos.component.ts
+++ ng-project/src/app/photos/photos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { PhotosService } from './photos.service';
+import { PhotosService } from '../services/photos.service';
 
 @Component({
   selector: 'app-photos',
@@ -10,13 +10,22 @@ import { PhotosService } from './photos.service';
 export class PhotosComponent implements OnInit {
   photos: any;
   loading = true;
+  errorMessage = '';
 
   constructor(private photoService: PhotosService) {}
 
   ngOnInit() {
-    this.photoService.getAllPhotos().subscribe(photos => {
-      this.photos = photos;
-      this.loading = false;
-    });
+    this.photoService.getAllPhotos().subscribe(
+      photos => {
+        this.photos = photos;
+        this.loading = false;
+      },
+      error => {
+        console.log('error retrieving photos:', error);
+        this.loading = false;
+        this.errorMessage =
+          'We were unable to retrieve images at this time. Please try again later.';
+      }
+    );
   }
 }
