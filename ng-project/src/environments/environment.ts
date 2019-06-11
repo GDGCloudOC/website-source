54740c0b1951318802fc49e26f2542f60a5e7fc6
Charlie Collier <charlie.collier@neudesic.com>
2019-06-11 14:27:33 -0700

    updated key and email account info

--- ng-project/src/environments/environment.ts
+++ ng-project/src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  meetupBaseUrl: 'https://cors-anywhere.herokuapp.com/https://api.meetup.com',
-  meetupGroupName: 'gdgcloudoc',
-  contactFormLink: 'https://usebasin.com/f/49362a2c0a33',
-  youtubeBaseUrl: 'https://www.googleapis.com/youtube/v3',
-  youtubeGDGId: 'UCGGpNEJD4nP5vtSuCbaKGBA',
-  youtubeEmbedBaseUrl: 'https://www.youtube-nocookie.com/embed',
+  meetupBaseUrl: "https://cors-anywhere.herokuapp.com/https://api.meetup.com",
+  meetupGroupName: "gdgcloudoc",
+  contactFormLink: "https://usebasin.com/f/7ef3301af4a0",
+  youtubeBaseUrl: "https://www.googleapis.com/youtube/v3",
+  youtubeGDGId: "UCGGpNEJD4nP5vtSuCbaKGBA",
+  youtubeEmbedBaseUrl: "https://www.youtube-nocookie.com/embed",
   embedVideoHeight: 480
 };
 
