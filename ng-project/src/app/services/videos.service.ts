d6c4f7a420b3134380e2b25c0141b4a424c34f22
Charlie Collier <charlie.collier@neudesic.com>
2019-06-11 15:19:13 -0700

    updating api info and usebasin endpoint

--- ng-project/src/app/services/videos.service.ts
+++ ng-project/src/app/services/videos.service.ts
@@ -1,29 +1,35 @@
-import { Injectable } from '@angular/core';
-import { youtubeApiKey } from '../../assets/apiKeys';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { Injectable } from "@angular/core";
+import { youtubeApiKey } from "./apiKeys";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class VideosService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getPlaylistId(): Observable<any> {
     return this.http.get<any>(
-      `${environment.youtubeBaseUrl}/channels?part=contentDetails&id=${environment.youtubeGDGId}&key=${youtubeApiKey}`
+      `${environment.youtubeBaseUrl}/channels?part=contentDetails&id=${
+        environment.youtubeGDGId
+      }&key=${youtubeApiKey}`
     );
   }
 
-  getVideosByPlaylistId(playlistId: string, pageToken?: string): Observable<any> {
+  getVideosByPlaylistId(
+    playlistId: string,
+    pageToken?: string
+  ): Observable<any> {
     return this.http.get<any>(
-      environment.youtubeBaseUrl
-      + '/playlistItems?part=contentDetails&playlistId='
-      + playlistId
-      + (pageToken ? '&pageToken=' + pageToken : '')
-      + '&maxResults=5'
-      + '&key=' + youtubeApiKey);
+      environment.youtubeBaseUrl +
+        "/playlistItems?part=contentDetails&playlistId=" +
+        playlistId +
+        (pageToken ? "&pageToken=" + pageToken : "") +
+        "&maxResults=5" +
+        "&key=" +
+        youtubeApiKey
+    );
   }
 }
